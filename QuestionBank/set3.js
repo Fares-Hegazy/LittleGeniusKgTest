977e178b5714e3ea6cb78770683717d651289eef
Fares-Hegazy <mhegazy1522@gmail.com>
2025-07-11 14:53:13 +0300

    Add files via upload

---
+++ QuestionBank/set3.js
@@ -0,0 +1,552 @@
+export const set3 = [
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  43</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["50", "52", "51", "53"],
+    answer: "52"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  65</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["72", "73", "71", "74"],
+    answer: "72"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  46</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["55", "56", "53", "54"],
+    answer: "54"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  84</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["91", "94", "92", "93"],
+    answer: "93"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  63</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["71", "72", "70", "73"],
+    answer: "71"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  67</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["73", "74", "75", "76"],
+    answer: "75"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  42</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["52", "50", "53", "51"],
+    answer: "51"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  34</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["41", "40", "42", "43"],
+    answer: "41"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  53</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["62", "61", "60", "59"],
+    answer: "60"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  68</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["74", "75", "73", "76"],
+    answer: "75"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  56</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["63", "64", "65", "62"],
+    answer: "63"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  38</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["46", "44", "47", "45"],
+    answer: "45"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  44</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["50", "51", "52", "53"],
+    answer: "52"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  86</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["94", "92", "95", "93"],
+    answer: "94"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  57</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["66", "65", "63", "64"],
+    answer: "64"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  28</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["35", "34", "33", "36"],
+    answer: "35"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  37</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["44", "45", "46", "47"],
+    answer: "45"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  29</div>
+          <div class="row"><span class="op">+</span>  5</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["33", "36", "34", "35"],
+    answer: "34"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  64</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["72", "71", "70", "73"],
+    answer: "71"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  26</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["32", "31", "34", "33"],
+    answer: "32"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  27</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["33", "36", "35", "34"],
+    answer: "34"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  38</div>
+          <div class="row"><span class="op">+</span>  4</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["42", "41", "44", "43"],
+    answer: "42"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  28</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["34", "36", "35", "37"],
+    answer: "36"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  49</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["58", "57", "56", "55"],
+    answer: "56"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  35</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["41", "40", "43", "42"],
+    answer: "41"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  33</div>
+          <div class="row"><span class="op">+</span>  4</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["39", "38", "36", "37"],
+    answer: "37"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  53</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["61", "62", "64", "63"],
+    answer: "62"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  47</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["55", "54", "57", "56"],
+    answer: "55"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  56</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["67", "66", "65", "64"],
+    answer: "65"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  42</div>
+          <div class="row"><span class="op">+</span>  5</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["49", "48", "46", "47"],
+    answer: "47"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  26</div>
+          <div class="row"><span class="op">+</span>  4</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["30", "31", "29", "32"],
+    answer: "30"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  23</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["32", "31", "33", "30"],
+    answer: "31"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  77</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["83", "84", "82", "85"],
+    answer: "83"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  76</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["82", "85", "83", "84"],
+    answer: "84"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  82</div>
+          <div class="row"><span class="op">+</span>  3</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["85", "84", "87", "86"],
+    answer: "85"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  45</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["54", "52", "53", "55"],
+    answer: "53"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  76</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["84", "83", "81", "82"],
+    answer: "82"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  64</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["72", "71", "74", "73"],
+    answer: "72"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  36</div>
+          <div class="row"><span class="op">+</span>  3</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["41", "38", "39", "40"],
+    answer: "39"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  38</div>
+          <div class="row"><span class="op">+</span>  5</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["43", "44", "42", "45"],
+    answer: "43"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  57</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["63", "65", "64", "66"],
+    answer: "65"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  84</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["92", "91", "90", "89"],
+    answer: "90"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  44</div>
+          <div class="row"><span class="op">+</span>  9</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["54", "53", "55", "52"],
+    answer: "53"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  88</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["96", "94", "97", "95"],
+    answer: "96"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  32</div>
+          <div class="row"><span class="op">+</span>  2</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["33", "34", "36", "35"],
+    answer: "34"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  43</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["51", "50", "53", "52"],
+    answer: "51"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  55</div>
+          <div class="row"><span class="op">+</span>  7</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["64", "63", "62", "61"],
+    answer: "62"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  68</div>
+          <div class="row"><span class="op">+</span>  8</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["74", "75", "76", "77"],
+    answer: "76"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  75</div>
+          <div class="row"><span class="op">+</span>  6</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["83", "80", "82", "81"],
+    answer: "81"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  56</div>
+          <div class="row"><span class="op">+</span>  2</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["57", "58", "59", "60"],
+    answer: "58"
+  }
+];
