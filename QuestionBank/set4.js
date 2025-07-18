0e70b975ddca0e96bf30ba3cc3ec5560c91b2738
Fares-Hegazy <mhegazy1522@gmail.com>
2025-07-18 18:40:25 +0300

    Update set4.js

--- QuestionBank/set4.js
+++ QuestionBank/set4.js
@@ -1,2 +1,11 @@
 export const set4 = [
+  { numb: 1, type: "Friend Number", question: "What is the friend number of 1?", answer: "9" },
+  { numb: 2, type: "Friend Number", question: "What is the friend number of 2?", answer: "8" },
+  { numb: 3, type: "Friend Number", question: "What is the friend number of 3?", answer: "7" },
+  { numb: 4, type: "Friend Number", question: "What is the friend number of 4?", answer: "6" },
+  { numb: 5, type: "Friend Number", question: "What is the friend number of 5?", answer: "5" },
+  { numb: 6, type: "Friend Number", question: "What is the friend number of 6?", answer: "4" },
+  { numb: 7, type: "Friend Number", question: "What is the friend number of 7?", answer: "3" },
+  { numb: 8, type: "Friend Number", question: "What is the friend number of 8?", answer: "2" },
+  { numb: 9, type: "Friend Number", question: "What is the friend number of 9?", answer: "1" }
 ];
