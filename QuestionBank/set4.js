1b18aff552bd53bfb7ab0ff7e178310c9fe73499
Fares-Hegazy <mhegazy1522@gmail.com>
2025-07-18 18:54:35 +0300

    Update set4.js

--- QuestionBank/set4.js
+++ QuestionBank/set4.js
@@ -1,11 +1,159 @@
 export const set4 = [
-  { numb: 1, type: "Friend Number", question: "What is the friend number of 1?", answer: "9" },
-  { numb: 2, type: "Friend Number", question: "What is the friend number of 2?", answer: "8" },
-  { numb: 3, type: "Friend Number", question: "What is the friend number of 3?", answer: "7" },
-  { numb: 4, type: "Friend Number", question: "What is the friend number of 4?", answer: "6" },
-  { numb: 5, type: "Friend Number", question: "What is the friend number of 5?", answer: "5" },
-  { numb: 6, type: "Friend Number", question: "What is the friend number of 6?", answer: "4" },
-  { numb: 7, type: "Friend Number", question: "What is the friend number of 7?", answer: "3" },
-  { numb: 8, type: "Friend Number", question: "What is the friend number of 8?", answer: "2" },
-  { numb: 9, type: "Friend Number", question: "What is the friend number of 9?", answer: "1" }
+    // Friend Number
+    { numb: 1, type: "Friend Number", question: "What is the friend number of 1?", answer: "9" },
+    { numb: 2, type: "Friend Number", question: "What is the friend number of 2?", answer: "8" },
+    { numb: 3, type: "Friend Number", question: "What is the friend number of 3?", answer: "7" },
+    { numb: 4, type: "Friend Number", question: "What is the friend number of 4?", answer: "6" },
+    { numb: 5, type: "Friend Number", question: "What is the friend number of 5?", answer: "5" },
+    { numb: 6, type: "Friend Number", question: "What is the friend number of 6?", answer: "4" },
+    { numb: 7, type: "Friend Number", question: "What is the friend number of 7?", answer: "3" },
+    { numb: 8, type: "Friend Number", question: "What is the friend number of 8?", answer: "2" },
+    { numb: 9, type: "Friend Number", question: "What is the friend number of 9?", answer: "1" },
+    
+    // Finite or Infinite
+    { numb: 10, type: "Finite or Infinite", question: "4 + 3 =", options: ["5", "6", "7", "8"], answer: "7", tag: "Finite" },
+    { numb: 11, type: "Finite or Infinite", question: "6 + 5 =", options: ["10", "11", "12", "13"], answer: "11", tag: "Infinite" },
+    { numb: 12, type: "Finite or Infinite", question: "8 + 4 =", options: ["11", "12", "13", "14"], answer: "12", tag: "Infinite" },
+    { numb: 13, type: "Finite or Infinite", question: "6 + 9 =", options: ["14", "15", "16", "17"], answer: "15", tag: "Infinite" },
+    { numb: 14, type: "Finite or Infinite", question: "7 + 9 =", options: ["15", "16", "17", "18"], answer: "16", tag: "Infinite" },
+    
+    // END Number
+    { numb: 15, type: "END Number", question: "9 + 7 =", options: ["15", "16", "17", "18"], answer: "16", end: "6" },
+    { numb: 16, type: "END Number", question: "5 + 6 =", options: ["10", "11", "12", "13"], answer: "11", end: "1" },
+    { numb: 17, type: "END Number", question: "4 + 8 =", options: ["12", "13", "14", "15"], answer: "12", end: "2" },
+    { numb: 18, type: "END Number", question: "5 + 8 =", options: ["10", "11", "12", "13"], answer: "13", end: "3" },
+    { numb: 19, type: "END Number", question: "7 + 7 =", options: ["13", "14", "15", "16"], answer: "14", end: "4" },
+
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  51</div>
+            <div class="row"><span class="op">+</span>  5</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["56", "55", "57", "58"],
+      answer: "56"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  34</div>
+            <div class="row"><span class="op">+</span>  3</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["36", "37", "38", "39"],
+      answer: "37"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  56</div>
+            <div class="row"><span class="op">+</span>  2</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["58", "57", "59", "60"],
+      answer: "58"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  84</div>
+            <div class="row"><span class="op">+</span>  2</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["85", "87", "86", "88"],
+      answer: "86"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  63</div>
+            <div class="row"><span class="op">+</span>  4</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["66", "68", "67", "69"],
+      answer: "67"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  23</div>
+            <div class="row"><span class="op">+</span>  6</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["29", "28", "30", "27"],
+      answer: "29"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  63</div>
+            <div class="row"><span class="op">+</span>  1</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["62", "64", "63", "65"],
+      answer: "64"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  32</div>
+            <div class="row"><span class="op">+</span>  4</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["35", "37", "36", "38"],
+      answer: "36"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  62</div>
+            <div class="row"><span class="op">+</span>  7</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["68", "70", "69", "71"],
+      answer: "69"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  83</div>
+            <div class="row"><span class="op">+</span>  2</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["85", "84", "86", "87"],
+      answer: "85"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  63</div>
+            <div class="row"><span class="op">+</span>  5</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["67", "68", "69", "70"],
+      answer: "68"
+    },
+    {
+      question: `
+          <div class="vertical-question">
+            <div class="row">  48</div>
+            <div class="row"><span class="op">+</span>  1</div>
+            <div class="line">_____</div>
+          </div>
+        `,
+      options: ["50", "49", "48", "47"],
+      answer: "49"
+    }
 ];
