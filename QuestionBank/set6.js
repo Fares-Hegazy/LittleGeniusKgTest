73ef8bbb6fea19b6f78d2a1929f99bb3ba50a67a
Fares-Hegazy <mhegazy1522@gmail.com>
2025-08-21 15:21:53 +0300

    Add files via upload

---
+++ QuestionBank/set6.js
@@ -0,0 +1,182 @@
+export const set6 = [
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 3 = </div>
+        </div>
+      `,
+    options: ["5", "6", "9", "2"],
+    answer: "6"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 7 = </div>
+        </div>
+      `,
+    options: ["5", "3", "7", "8"],
+    answer: "7"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 8 = </div>
+        </div>
+      `,
+    options: ["16", "6", "9", "4"],
+    answer: "16"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 3 = </div>
+        </div>
+      `,
+    options: ["6", "9", "3", "1"],
+    answer: "3"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 6 = </div>
+        </div>
+      `,
+    options: ["7", "4", "12", "8"],
+    answer: "12"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 9 = </div>
+        </div>
+      `,
+    options: ["7", "9", "2", "5"],
+    answer: "9"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 1 = </div>
+        </div>
+      `,
+    options: ["4", "2", "7", "9"],
+    answer: "2"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 5 = </div>
+        </div>
+      `,
+    options: ["3", "8", "5", "1"],
+    answer: "5"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 10 = </div>
+        </div>
+      `,
+    options: ["5", "20", "9", "6"],
+    answer: "20"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 2 = </div>
+        </div>
+      `,
+    options: ["5", "2", "8", "3"],
+    answer: "2"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 4 = </div>
+        </div>
+      `,
+    options: ["3", "7", "8", "4"],
+    answer: "8"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 8 = </div>
+        </div>
+      `,
+    options: ["4", "9", "6", "8"],
+    answer: "8"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 7 = </div>
+        </div>
+      `,
+    options: ["9", "14", "5", "3"],
+    answer: "14"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 4 = </div>
+        </div>
+      `,
+    options: ["7", "2", "5", "4"],
+    answer: "4"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 2 = </div>
+        </div>
+      `,
+    options: ["8", "3", "4", "1"],
+    answer: "4"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 10 = </div>
+        </div>
+      `,
+    options: ["10", "3", "6", "8"],
+    answer: "10"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 5 = </div>
+        </div>
+      `,
+    options: ["9", "10", "6", "3"],
+    answer: "10"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 6 = </div>
+        </div>
+      `,
+    options: ["9", "6", "4", "2"],
+    answer: "6"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">2 x 9 = </div>
+        </div>
+      `,
+    options: ["8", "3", "18", "7"],
+    answer: "18"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">1 x 1 = </div>
+        </div>
+      `,
+    options: ["1", "4", "7", "9"],
+    answer: "1"
+  }
+];
