6ee96e991b42b6c8aa075eb1c1cc93e7a20f1911
Fares-Hegazy <mhegazy1522@gmail.com>
2025-09-13 19:39:45 +0300

    Add files via upload

---
+++ QuestionBank/set7.js
@@ -0,0 +1,227 @@
+export const set7 = [
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 8 = </div>
+        </div>
+      `,
+    options: ["32", "28", "36", "24"],
+    answer: "32"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 8 = </div>
+        </div>
+      `,
+    options: ["21", "27", "24", "29"],
+    answer: "24"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 4 = </div>
+        </div>
+      `,
+    options: ["6", "8", "9", "7"],
+    answer: "8"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">5 x 6 = </div>
+        </div>
+      `,
+    options: ["35", "25", "28", "30"],
+    answer: "30"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 5 = </div>
+        </div>
+      `,
+    options: ["15", "12", "18", "13"],
+    answer: "15"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">5 x 5 = </div>
+        </div>
+      `,
+    options: ["20", "25", "28", "23"],
+    answer: "25"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 9 = </div>
+        </div>
+      `,
+    options: ["24", "30", "27", "21"],
+    answer: "27"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 6 = </div>
+        </div>
+      `,
+    options: ["28", "20", "22", "24"],
+    answer: "24"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 5 = </div>
+        </div>
+      `,
+    options: ["12", "10", "8", "14"],
+    answer: "10"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 9 = </div>
+        </div>
+      `,
+    options: ["36", "32", "38", "34"],
+    answer: "36"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 7 = </div>
+        </div>
+      `,
+    options: ["16", "12", "14", "18"],
+    answer: "14"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">6 x 3 = </div>
+        </div>
+      `,
+    options: ["15", "18", "21", "12"],
+    answer: "18"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">5 x 7 = </div>
+        </div>
+      `,
+    options: ["32", "35", "38", "30"],
+    answer: "35"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 6 = </div>
+        </div>
+      `,
+    options: ["10", "14", "12", "15"],
+    answer: "12"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">9 x 7 = </div>
+        </div>
+      `,
+    options: ["60", "63", "66", "69"],
+    answer: "63"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 13 = </div>
+        </div>
+      `,
+    options: ["26", "24", "28", "22"],
+    answer: "26"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 24 = </div>
+        </div>
+      `,
+    options: ["92", "96", "88", "84"],
+    answer: "96"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">5 x 31 = </div>
+        </div>
+      `,
+    options: ["152", "155", "158", "150"],
+    answer: "155"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 23 = </div>
+        </div>
+      `,
+    options: ["66", "72", "69", "63"],
+    answer: "69"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 52 = </div>
+        </div>
+      `,
+    options: ["204", "208", "212", "216"],
+    answer: "208"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 53 = </div>
+        </div>
+      `,
+    options: ["159", "156", "162", "153"],
+    answer: "159"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 42 = </div>
+        </div>
+      `,
+    options: ["164", "172", "168", "176"],
+    answer: "168"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">4 x 34 = </div>
+        </div>
+      `,
+    options: ["132", "136", "140", "128"],
+    answer: "136"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">2 x 27 = </div>
+        </div>
+      `,
+    options: ["54", "56", "52", "58"],
+    answer: "54"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <div class="row">3 x 48 = </div>
+        </div>
+      `,
+    options: ["141", "147", "144", "138"],
+    answer: "144"
+  }
+];
